9d9654e7e1384e8ab3982a42aa25060dd3f63c3e
opilane <opilane@HP-CZC2349HTZ>
2024-03-14 15:54:44 +0200

    Add project files.

---
+++ MyModul.py
@@ -0,0 +1,20 @@
+#kasutaja=["Valeria","Dasha","Marina","Masha","Sasha"]
+def registreerimine(kasutaja=["Valeria","Dasha","Marina","Masha","Sasha"]) :
+    """
+    """
+r=input("Tahad registreerida? 1->Jah, 0->Ei")
+if r=="1":
+    k=input("Sisestage oma kasutajanimi :")  #k->kasutajanimi
+    p=input("Sisestage oma parooli :")       #p->parooli
+#    print()
+#while True :
+    if k=="Valeria, Dasha, Marina, Masha, Sasha" :
+        print("See kasutajanimi on juba olemas! Proovi uuesti!")
+    elif :
+        
+
+
+    #else :
+    #    vastus_1=input("Tahad autoriseerida? 3->Jah, 4->Ei")
+    #    print()
+    
---
+++ Regitreerimine_ja_autoriseerimine.py
@@ -0,0 +1,6 @@
+from MyModul import *
+r=input("Tahad registreerida?") #r->registreerimine
+if r=="jah" :
+    print("Palun registreeruge!")
+user=input("Sisesta kasutajanimi :")
+
