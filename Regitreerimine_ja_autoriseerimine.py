d1bdb009e83f48430b5e7b52b88b44dfba5a84e6
allikvaleria <154507539+allikvaleria@users.noreply.github.com>
2024-03-18 15:07:29 +0200

    Update Regitreerimine_ja_autoriseerimine.py

--- Regitreerimine_ja_autoriseerimine.py
+++ Regitreerimine_ja_autoriseerimine.py
@@ -1,6 +1,17 @@
 from MyModul import *
-r=input("Tahad registreerida?") #r->registreerimine
-if r=="jah" :
-    print("Palun registreeruge!")
-user=input("Sisesta kasutajanimi :")
-
+kasutaja=["Valeria","Dasha","Marina","Masha","Sasha"]
+paroolid=["va123leria","dasha543","marina.o777","masha.0908","sasha#333"]
+while True :
+    vastus=int(input("1-registreerimine/n2-autoriseerimine/n3-nime või parooli muutmine/n4-unustanud parooli taastamine/n5-lõpetamine./n")) #4-nime või parooli muutmine/n5-unustanud parooli taastamine/n
+    if vastus==1:
+        registreerimine(kasutaja,paroolid)
+    if vastus==2:
+        autoriseerimine(kasutaja,paroolid)
+    if vastus==3 :
+        print(kasutaja,paroolid)
+        nime_või_parooli_muutmine(kasutaja,paroolid)
+    #    print("Head aega!")
+    #if vastus==4 :
+    #    nvpm=input("Mida sa tahad muutumine?/nN-nime/nP-parooli")
+    #    print()
+    #if vastus=="N" 
